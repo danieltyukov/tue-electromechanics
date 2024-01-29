bf46fff251635cb93ca649355035acda3aa4220f
linux-hp <k.lo.kam.y.minh.khanh@student.tue.nl>
2024-01-29 12:43:20 +0100

    add median absolute error calculation

---
+++ Tyukov_1819283/Tyukov_1819283_Matlab/median_absolute_error.m
@@ -0,0 +1,16 @@
+function median_absolute_error = median_absolute_error(simulated_x, measured_x, simulated_y, measured_y, tolerance)
+    errors = [];
+    test_x = [];
+    test_y = [];
+    for i = 1:length(measured_x)
+        ind = find(abs(simulated_x - measured_x(i)) <= tolerance);
+        
+        % Loop through all matching simulated_x values
+        for j = 1:length(ind)
+            error = 100*abs((simulated_y(ind(j)) - measured_y(i))./measured_y(i));
+            errors = [errors, error]; % Append to errors array
+        end
+    end
+    % Calculate median absolute error
+    median_absolute_error = median(errors);
+end
